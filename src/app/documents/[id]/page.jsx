4f67060e4a87e0e4f21212f759f8ca2ba04152ef
himanshux88 <himanshujindal201@gmail.com>
2026-03-20 12:49:13 +0530

    feat(realtime): add real-time document sync using Supabase subscriptions

--- src/app/documents/[id]/page.jsx
+++ src/app/documents/[id]/page.jsx
@@ -3,12 +3,41 @@
 import { getDocumentsById, updateDocument } from "features/documents/api";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { supabase } from "services/supabaseClient";
 
 export default function DocumentPage() {
   const { id } = useParams();
   const [document, setDocument] = useState(null);
   const [content, setContent] = useState("");
   const [saving, setSaving] = useState(false);
+  const [isLocalChange, setIsLocalChange] = useState(false);
+
+  useEffect(() => {
+    if (!id) return;
+
+    const channel = supabase
+      .channel("realtime-doc")
+      .on(
+        "postgres_changes",
+        {
+          event: "UPDATE",
+          schema: "public",
+          table: "documents",
+          filter: `id=eq.${id}`,
+        },
+        (payload) => {
+          if (!isLocalChange) {
+            console.log("Realtime Update:", payload);
+            setContent(payload.new.content);
+          }
+        },
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [id]);
 
   useEffect(() => {
     const fetchDoc = async () => {
@@ -21,15 +50,16 @@ export default function DocumentPage() {
   }, [id]);
 
   useEffect(() => {
-    if (!document) return;
+    if (!document || !isLocalChange) return;
 
     setSaving(true);
     const timeout = setTimeout(async () => {
       await updateDocument(id, content);
+      setIsLocalChange(false);
       setSaving(false);
     }, 1000);
     return () => clearTimeout(timeout);
-  }, [content]);
+  }, [content, isLocalChange]);
 
   if (!document) return <div>Loading...</div>;
   return (
@@ -39,7 +69,10 @@ export default function DocumentPage() {
       <textarea
         className="w-full h-100 border p-4"
         value={content}
-        onChange={(e) => setContent(e.target.value)}
+        onChange={(e) => {
+          setContent(e.target.value);
+          setIsLocalChange(true);
+        }}
       />
       <p className="text-sm text-gray-500">{saving ? "Saving..." : "Saved"}</p>
     </div>
